0dbb489caeb6db24a8f9f63ea663b38e73d530f1
MohamedDIOUF <mohameddiouf096@gmail.com>
2025-11-11 12:26:01 +0000

    okayy

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:8080',
-      'https://work-n-life-front-eo9f3yyvp-modioufs-projects.vercel.app'
+      'https://work-n-life-front-eo9f3yyvp-modioufs-projects.vercel.app',
+      'http://localhost:8080', 
     ], // ou le port de ton front React
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
